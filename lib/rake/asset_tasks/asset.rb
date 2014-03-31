202faa8165e2f0b0c05294ed9c465c49f2679b00
Jason Waldrip <jason@waldrip.net>
2014-03-30 19:00:36 -0600

    fix new rubocop errors

--- lib/rake/asset_tasks/asset.rb
+++ lib/rake/asset_tasks/asset.rb
@@ -1,36 +1,38 @@
 # Rake Extensions
 module Rake
-  # Asset class for sprockets pre-compilation
-  class AssetTasks::Asset
-    # Returns a sprockets asset
-    # @param env [Sprockets::Environment]
-    # @param file [String]
-    def initialize(env, file)
-      @env  = env
-      @file = file
-    end
+  class AssetTasks
+    # Asset class for sprockets pre-compilation
+    class Asset
+      # Returns a sprockets asset
+      # @param env [Sprockets::Environment]
+      # @param file [String]
+      def initialize(env, file)
+        @env  = env
+        @file = file
+      end
 
-    # Returns the path of an asset
-    # @return [String]
-    def path
-      File.expand_path(file).sub(/(#{@env.paths.join('|')})\//, '')
-    end
+      # Returns the path of an asset
+      # @return [String]
+      def path
+        File.expand_path(file).sub(/(#{@env.paths.join('|')})\//, '')
+      end
 
-    private
+      private
 
-    def basename
-      File.basename @file
-    end
+      def basename
+        File.basename @file
+      end
 
-    def parts
-      basename.split('.').size
-    end
+      def parts
+        basename.split('.').size
+      end
 
-    def file
-      if parts > 2
-        @file.split('.').tap(&:pop).join('.')
-      else
-        @file
+      def file
+        if parts > 2
+          @file.split('.').tap(&:pop).join('.')
+        else
+          @file
+        end
       end
     end
   end
