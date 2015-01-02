cc1c28568dcc3378748bdccb6c54d6cc20a9a801
Jason Waldrip <jason@waldrip.net>
2015-01-02 14:43:41 -0700

    if statement support

--- lib/flatrack/middleware.rb
+++ lib/flatrack/middleware.rb
@@ -1,12 +1,17 @@
 class Flatrack
   class Middleware
 
-    def initialize(app, flatrack_app: Flatrack::Site)
+    def initialize(app, opts = {})
+      @if      = opts[:if] || Proc.new { true }
+      @unless  = opts[:unless] || Proc.new { false }
       @app          = app
-      @flatrack_app = flatrack_app
+      @flatrack_app = opts[:flatrack_app] || Flatrack::Site
     end
 
     def call(env)
+      Flatrack::DomainParser.new(null_app).call(env)
+      allow = @if.call(env) && !@unless.call(env)
+      return call_app env unless allow
       response = @flatrack_app.call env
       status, _, _ = response
       response     = call_app(env) if status == 404
@@ -17,6 +22,10 @@ class Flatrack
 
     def call_app(env)
       @app.call env
+    end
+
+    def null_app
+      ->(_){}
     end
 
   end
